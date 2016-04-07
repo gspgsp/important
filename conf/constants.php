2d74bda745c13872331c234db183809a588ae608
lixian <lixian@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2016-04-07 08:17:36 +0000

    添加前段静态页面模板

--- conf/constants.php
+++ conf/constants.php
@@ -1,6 +1,5 @@
 <?php
 
 define('APP_DEBUG',true);
-define('APP_URL', 'http://www.wdsln.com');
-define('WAP_URL', 'http://m.wdsln.com');
-define('FILE_URL', 'http://static.wdsln.com');
+define('APP_URL', 'http://www.online.com');
+define('FILE_URL', 'http://static.online.com');
