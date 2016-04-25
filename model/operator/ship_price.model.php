4a96c617d89ea684b87e06e293b9deab904e3480
lixian <lixian@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2016-04-25 02:08:22 +0000

    资源库前端代码

--- model/operator/ship_price.model.php
+++ model/operator/ship_price.model.php
@@ -2,7 +2,7 @@
 /**
  * 资源库信息
  */
-class shipPriceModel extends model{
+class ship_priceModel extends model{
 	public function __construct() {
 		parent::__construct(C('db_default'), 'ship_price');
 	}
