6c01af94ada120da52eaed7a17a6576408bcf63c
cuiyinming <cuiyinming@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2016-04-29 08:09:11 +0000

    联系人信息更新

--- model/rbac/adm.model.php
+++ model/rbac/adm.model.php
@@ -11,12 +11,22 @@ class admModel extends model{
 	 * 取得当前用户的所有权限列表
 	 * @access public
 	 * @param int $admin_id 用户ID
-     * @return array
+	 * @return array
 	 */
 	public function getUserById($admin_id=0) {
 		//获取所有非超管
 		$nodes=$this->model('admin')->select('admin_id,username')->where('admin_id in('.$admin_id.')')->getAll();
 		return $nodes;
-    }
+	}
+	/*
+	 * 取得指定字段内容
+	 * @access public
+	 * @param int $admin_id 用户ID
+	 * @return array
+	 */
+	public function getUserByCol($admin_id=0,$col='name') {
+		return $this->model('admin')->select("$col")->where("admin_id = ".$admin_id)->getOne();
+	}
+
 }
 ?>
