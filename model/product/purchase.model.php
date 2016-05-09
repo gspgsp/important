f4760dd5e5f38291269e2642d28ce9ccc0767e65
zhanghao <zhanghao@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2016-05-09 08:04:59 +0000

    订单模型 auto release

---
+++ model/product/purchase.model.php
@@ -0,0 +1,13 @@
+<?php 
+//订单模型
+class purchaseModel extends model{
+	public function __construct() {
+		parent::__construct(C('db_default'), 'purchase');
+	}
+	//根据pid获取指定的字段值
+	public function getColById($pid=0,$col='id'){
+		$result=$this->select("$col")->where('p_id='.$pid)->getOne();
+		return $result>0 ? $result : 0;
+	}
+
+}
