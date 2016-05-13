19f2a24b56a687899861a984e2054478509da576
lixian <lixian@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2016-05-13 06:28:07 +0000

    info auto release

---
+++ module/touch/information.action.php
@@ -0,0 +1,25 @@
+<?php
+/**
+*塑料头条控制器
+*/
+class informationAction extends homeBaseAction
+{
+
+	public function __init() {
+		$this->debug = false;
+		$this->db=M('public:common')->model('info');
+    }
+    //返回资源的内容
+    public function get_items(){
+    	//获取资源的类型
+		$pid=sget('pid','i',0);
+		$page = sget('page','i',0);
+		$size = sget('size','i',10);
+		$result = array();
+		if($pid>0){
+			$result = M('touch:infos')->getCateList($pid,$page,$size);
+		}
+		$this->json_output($result);
+	}
+
+}
