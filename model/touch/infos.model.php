aa28502c9a96317a91b8f2b59591316e0b77d6f8
lixian <lixian@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2016-05-13 06:28:57 +0000

    infosmodel auto release

---
+++ model/touch/infos.model.php
@@ -0,0 +1,24 @@
+<?php
+/**
+*资讯模型
+*/
+class infosModel extends model
+{
+	public function __construct() {
+		parent::__construct(C('db_default'), 'info');
+	}
+	//获取对应的文章
+	public function getCateList($cid,$page=1,$size=20,$sortField='input_time',$sortOrder='desc'){
+		$_key='article_list';
+		$cache=cache::startMemcache();
+		$data=$cache->get($_key);
+		if(empty($data)){
+			$list = $this->model('info')->select('id,title,img,cate_id,content,description,input_time,update_time')->where('cate_id='.$cid)
+			->page($page+1,$size)
+			->order("$sortField $sortOrder")
+			->getPage();
+		$cache->set($_key,$data,86400);//加入缓存
+		}
+		return $list;
+	}
+}
