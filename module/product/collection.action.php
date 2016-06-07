b6058d0f52d297768f7f2ee2afd843a47496deab
xianghui <xianghui@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2016-06-07 13:06:45 +0000

    修改数值不符合bug

--- module/product/collection.action.php
+++ module/product/collection.action.php
@@ -89,7 +89,6 @@ class collectionAction extends adminBaseAction
 			$list['data'][$k]['input_time']=$v['input_time']>1000 ? date("Y-m-d H:i:s",$v['input_time']) : '-';
 			$list['data'][$k]['update_time']=$v['update_time']>1000 ? date("Y-m-d H:i:s",$v['update_time']) : '-';
 			$list['data'][$k]['payment_time']=$v['payment_time']>1000 ? date("Y-m-d H:i:s",$v['payment_time']) : '-';
-			$list['data'][$k]['price']=$v['total_price']-$v['uncollected_price'];
 		}
 		$result=array('total'=>$list['count'],'data'=>$list['data'],'msg'=>'');
 		$this->json_output($result);	
