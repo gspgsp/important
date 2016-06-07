f62b550e228f2a02142720646394ae734027de67
lixian <lixian@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2016-06-07 08:52:27 +0000

    个人中心密码修改

---
+++ module/user/changepass.action.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * 修改密码--黎贤
+ */
+class changepassAction extends homeBaseAction{
+
+
+	public function init()
+	{
+		if($_POST){
+			$this->is_ajax=true;
+			$old=trim(sget('old','s',''));
+			$new=trim(sget('new','s',''));
+			$renew=trim(sget('renew','s',''));
+			if($old==''||$new==''||$renew=='') $this->error('请输入密码');
+			if( strlen($old)<6||strlen($old)>16 ) $this->error('密码长度为6-16位字符');
+			if( strlen($new)<6||strlen($new)>16 ) $this->error('密码长度为6-16位字符');
+			if( $new!=$renew ) $this->error('两次密码不一致');
+			$model=M("user:customerContact");
+			$uinfo=$model->where("user_id=$this->user_id")->getRow();
+			if(empty($uinfo)) $this->error('错误的账号');
+			$npassword=M('system:sysUser')->genPassword($old.$uinfo['salt']);
+			if($uinfo['password']!==$npassword){
+				$this->error('错误的密码');
+			}
+			$newpass=M('system:sysUser')->genPassword($new.$uinfo['salt']);
+			$_data=array(
+				'password'=>$newpass,
+				'update_time'=>CORE_TIME,
+			);
+			if(!$model->where("user_id=$this->user_id")->update($_data)) $this->error('操作失败，系统错误');
+			$this->success('操作成功');
+
+		}else{
+			$this->display('changepass');
+		}
+		
+	}
+}
--- module/user/myset.action.php
+++ module/user/myset.action.php
@@ -12,6 +12,7 @@ class mysetAction extends homeBaseAction{
 
 	public function init()
 	{
+		$this->act="myset";
 		$user_info=$this->db->from('customer_contact c')
 			->join('contact_info i','c.user_id=i.user_id')
 			->leftjoin('admin ad','c.customer_manager=ad.admin_id')
@@ -19,7 +20,19 @@ class mysetAction extends homeBaseAction{
 			->select("c.user_id,c.mobile,c.name,c.tel,c.fax,c.c_id,email,i.points,i.thumb,ad.name as ad_name")
 			->getRow();
 		// p($user_info);
+
+		//公司信息
+		$customer=$this->db->model('customer')->where("c_id={$user_info['c_id']}")->getRow();
+
+		$this->area=M('system:region')->get_regions(1);
+
+		$origin=explode('|',$customer['origin']);
+		if($origin[0]){
+			$this->city=M('system:region')->get_regions($origin[0]);
+		}
+		$this->assign('origin',$origin);
 		$this->assign('user_info',$user_info);
+		$this->assign('customer',$customer);
 		$this->display('myset');
 	}
 
@@ -42,4 +55,28 @@ class mysetAction extends homeBaseAction{
 			$this->success('修改成功');
 		}
 	}
+
+	public function edit_company()
+	{
+		if($_POST){
+			$this->is_ajax=true;
+			$model=$this->db->model('customer');
+			$_data=saddslashes($_POST);
+			$c_id=$_SESSION['uinfo']['c_id'];//用户关联客户id
+			if(!$customer=$model->where("c_id=$c_id")->getRow()) $this->error('信息不存在');
+			if(!trim($_data['c_name'])) $this->error('公司名称不能为空');
+			if(!trim($_data['need_product'])) $this->error('需求牌号不能为空');
+			if(!trim($_data['address'])) $this->error('公司详细地址不能为空');
+			if(!$_data['origin']) $this->error('公司地址不能为空');
+			$c_name=trim($_data['c_name']);
+			if($c_name!=$customer['c_name'] && $model->where("c_name='{$c_name}'")->getOne()) $this->error('公司已存在，不能重复新建。');
+			if($customer['contact_id']!=$this->user_id) $this->error('不是默认联系人不能修改');
+			$_data['update_time']=CORE_TIME;
+			$_data['origin']=implode('|',$_data['origin']);
+			if(!$model->where("c_id=$c_id")->update($_data)) $this->error('操作失败，系统错误。');
+			$this->success('操作成功');
+		}
+	}
+
+
 }
