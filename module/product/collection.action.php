4f11de0f7e85fb8e85dfb7060cd02f209bb1ac5b
xianghui <xianghui@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2016-06-20 06:40:16 +0000

    下载

--- module/product/collection.action.php
+++ module/product/collection.action.php
@@ -172,4 +172,47 @@ class collectionAction extends adminBaseAction
 		}
 	}
 
+	/**
+	 * 下载附件
+	 * @access private 
+	 */
+	public function downloadAdjunct(){
+		$this->is_ajax=true; //指定为Ajax输出
+		$data = sdata(); //获取UI传递的参数
+		if(empty($data)) $this->error('错误的操作');
+		
+		$uptype=C('upload_type');
+		$cfg=C('upload_'.$uptype);
+
+		header("Content-type:text/html;charset=utf-8");
+		//用以解决中文不能显示出来的问题
+		$file_name=iconv("utf-8","gb2312",$data['accessory']);//16/06/16/57629c0249c65.doc
+		$file_sub_path=$cfg['path']; //D:\xampp\htdocs\svnonline\branches\cp/../static/upload/
+
+		$file_path=$file_sub_path.$file_name;
+		//首先要判断给定的文件存在与否
+		if(!file_exists($file_path)){
+			echo "没有该文件";
+			return ;
+		}
+		$fp=fopen($file_path,"r");
+		$file_size=filesize($file_path);//22
+
+		//下载文件需要用到的头
+		Header("Content-type: application/octet-stream");
+		Header("Accept-Ranges: bytes");
+		Header("Accept-Length:".$file_size);
+		Header("Content-Disposition: attachment; filename=".$file_name);
+		$buffer=1024;
+		$file_count=0;
+		//向浏览器返回数据
+		while(!feof($fp) && $file_count<$file_size){
+		$file_con=fread($fp,$buffer);
+		$file_count+=$buffer;
+		echo $file_con;
+		}
+		fclose($fp);
+
+	}
+
 }
