a3304884b099aae760e59c3743dea2f74c1103c5
lixian <lixian@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2016-06-20 09:05:33 +0000

    报价单excel导入，工作台移植

--- lang/msg.lang.php
+++ lang/msg.lang.php
@@ -12,7 +12,8 @@ return array(
 		'offers'=>"您的%s：%s（牌号：%s，价格：%s）被 %s 用户采购，<a href='/user/myoffers/msgToList?id=%s&type=%s'>查看详情</a>",
 		//下自营订单
 		'order'=>"您已选购：牌号：%s 价格：%s ，生成自营订单，交易员审核后，将会生成正式订单，<a href='/user/selforder/detail/id/%s'>查看详情</a>",
-		
+		//联营订单
+		'union_order'=>"您的供货：%s，%s 已经被选中并生成联营订单，交易员审核后，将会生成正式订单，<a href='/user/unionorder/detail/id/%s'>查看详情</a>",
 	),
 
 	//短信模板
