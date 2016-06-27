9069b52bc456ca7fd4f9aa5d61b28ccce0b05a49
yuanjiaye <yuanjiaye@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2016-06-27 07:37:02 +0000

    修改个人中心连营 自营订单信息

--- module/user/index.action.php
+++ module/user/index.action.php
@@ -16,14 +16,32 @@ class indexAction extends userBaseAction{
         $list=M('user:customerContact')->getCustomerInFoById($userid);
 
         //我的关注列表
+        $where="user_id=$this->user_id and ";
+        $data = M('product:concernedProduct')->getConcernedList($where);
 
-        //$data = M('user:productAttentions')->getAttentionvalue();
-      //  p($data);
         $this->assign('data',$list);
+        //近三个月的订单信息(自营)
+        $date= date(strtotime('-90 day'));
+        $uid=$this->user_id;
+        $res1=M('product:order')->getOrderStatusInFo($uid,$date);//待审核
+        $res2=M('product:order')->getOrder($uid,$date);          //代付款
+        $res3=M('product:order')->getInvoice($uid,$date);        //带开票
+        $res4=M('product:order')->getOrderCancel($uid,$date);    //已取消
+        //近三个月的订单信息(联营)
+        $info1=M('product:unionOrder')->getOrderStatusInFo($uid,$date);//待审核
+        $info2=M('product:unionOrder')->getOrder($uid,$date);          //代付款
+        $info3=M('product:unionOrder')->getInvoice($uid,$date);        //带开票
+        $info4=M('product:unionOrder')->getOrderCancel($uid,$date);    //已取消
+        $this->assign('data1',$res1);
+        $this->assign('data2',$res2);
+        $this->assign('data3',$res3);
+        $this->assign('data4',$res4);
+        $this->assign('info1',$info1);
+        $this->assign('info2',$info2);
+        $this->assign('info3',$info3);
+        $this->assign('info4',$info4);
 
-
-
-		$this->display('index');
+        $this->display('index');
 	}
 
 //    //获取关注的列表
