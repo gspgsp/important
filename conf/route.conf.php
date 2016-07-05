05964b39c6fcca8d248c5a083dc93103ccb6e35e
lixian <lixian@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2016-07-05 03:04:45 +0000

    添加资讯路由，原旧网站文章导入

--- conf/route.conf.php
+++ conf/route.conf.php
@@ -11,13 +11,14 @@ return array(
 	//正则路由:规则
 	'ROUTE_RULE'=>array(
 		//正则表达式=> [模块/控制器/操作?]参数1=值1&参数2=值2...
-		'/^item\/([0-9]+)$/' => 'list/item/detail?id=$1',
-		'/^info\/([0-9]+)$/' => 'list/info/show?id=$1',
-		'/^(about|contact|mobile|safe|agreement|help)\/*(\w*)$/' => 'home/page/$1?id=$2',
-		'/^(index|ajax)(\/\w+)$/' => 'home/$1$2',
-		'/^(android|ios|wp|weixin)\/(\w+)\/(\w+)$/' => 'app/$2/$3?chanel=$1',
-		'/^(\w+\/\w+\/)(\w+)\-(\w+)/' => '$1$2$3',
-		'/^company\/([0-9]+)$/'=>'user/myHomeWeb/clickMyWeb?id=$1',
+		// '/^item\/([0-9]+)$/' => 'list/item/detail?id=$1',
+		// '/^info\/([0-9]+)$/' => 'list/info/show?id=$1',
+		// '/^(about|contact|mobile|safe|agreement|help)\/*(\w*)$/' => 'home/page/$1?id=$2',
+		// '/^(index|ajax)(\/\w+)$/' => 'home/$1$2',
+		// '/^(android|ios|wp|weixin)\/(\w+)\/(\w+)$/' => 'app/$2/$3?chanel=$1',
+		// '/^(\w+\/\w+\/)(\w+)\-(\w+)/' => '$1$2$3',
+		'/^mycompany\/([0-9]+)$/'=>'mycompany/index/init?id=$1',//我的网站子网站路由
+		'/^article\/show\/([0-9]+)$/'=>'article/index/info?id=$1',
 	),
 
 	//子(泛)域名路由规则
