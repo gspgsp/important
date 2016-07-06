d8ecb7b78179c6bd8221b2ca81a54fa1d311c4dc
lixian <lixian@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2016-07-06 01:25:51 +0000

    修改资讯新闻

--- module/article/index.action.php
+++ module/article/index.action.php
@@ -3,6 +3,7 @@ class IndexAction extends homeBaseAction{
 
 	protected $db;
 	public function __init(){
+
 		//资讯分类
 		$this->cate=M('system:cate')->where("pid=23")->select('cate_id,cate_name')->getAll();
 		
@@ -11,6 +12,9 @@ class IndexAction extends homeBaseAction{
 		$this->newOffers=$purchase->getPurPage("type=2 and pur.shelve_type=1 and  pur.cargo_type=1 and pur.status in (2,3,4)");
 		$this->newPurchase=$purchase->getPurPage("type=2 and pur.shelve_type=1 and  pur.cargo_type=2 and pur.status in (2,3,4)");
 		$this->db=M('public:common');
+
+
+
 	}
 	//
 	public function init()
@@ -18,23 +22,23 @@ class IndexAction extends homeBaseAction{
 		//资讯id
 		if(!$cate_id=sget('cid','i',0)) $cate_id=$this->cate[0]['cate_id'];
 		//分页
+		
 		$page=sget('page','i',1);
 		$page_size=10;
 		//获取资讯分页列表
 		$list=M('system:info')->getListByCate($cate_id,$page,$page_size);
 		$this->pages = pages($list['count'], $page, $page_size);
-
 		$this->assign('list',$list);
 		$this->assign('cid',$cate_id);
-		$this->display('index');
-		
+		$this->display('index.html');
+
 	}
 
 	public function info()
 	{
 		$id=sget('id','i',0);
 		$this->data=M('system:info')->getInfoById($id);
-		$this->display('info');
+		$this->display('info.html');
 	}
 
 	
