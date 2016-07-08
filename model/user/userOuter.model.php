ea2437b649d938a48a04d415bfca032284679230
lixian <lixian@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2016-07-08 01:29:32 +0000

    修改qq登录

--- model/user/userOuter.model.php
+++ model/user/userOuter.model.php
@@ -9,20 +9,17 @@ class userOuterModel extends model
 	}
 
 	//第三方授权登录绑定账号
-	public function bindUser($user_id){
-		$openid=$_SESSION['auth_openid'];
+	public function bindUser($user_id,$openid,$auth_info){
 		if($this->where("outer_id='{$openid}' or user_id=$user_id")->getRow()) return false;
 		$_data=array(
 			'outer_id'=>$openid,
 			'user_id'=>$user_id,
-			'outer_source'=>$_SESSION['auth_info']['type'],
-			'outer_name'=>$_SESSION['auth_info']['name'],
-			'outer_avatar'=>$_SESSION['auth_info']['head'],
+			'outer_source'=>$auth_info['type'],
+			'outer_name'=>$auth_info['name'],
+			'outer_avatar'=>$auth_info['head'],
 			'input_time'=>CORE_TIME,
 		);
 		$this->add($_data);
-		unset($_SESSION['auth_openid']);
-		unset($_SESSION['auth_info']);
 		return true;
 	}
 }
