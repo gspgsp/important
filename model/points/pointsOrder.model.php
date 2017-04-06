31f68f4810e8b34dee8d17d649afec1cb4abd6e5
xielei <xielei@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-04-06 09:44:45 +0000

    新的积分兑换接口 --xielei

--- model/points/pointsOrder.model.php
+++ model/points/pointsOrder.model.php
@@ -1,10 +1,58 @@
 <?php
+
 /**
  * 积分商品订单
  */
-class pointsOrderModel extends Model{
+class pointsOrderModel extends Model
+{
+    protected $cache;
 
-	public function __construct() {
-		parent::__construct(C('db_default'), 'points_order');
-	}
+    public function __construct()
+    {
+        parent::__construct(C('db_default'), 'points_order');
+    }
+
+    public function get_supply_demand_top($goods_id)
+    {
+        $cache = E('RedisCluster', APP_LIB . 'class');
+        $key = 'msg_top:' . $goods_id;
+        $pur_id = $cache->get($key);
+        if (!empty($pur_id)) {
+            return $pur_id;
+        } else {
+            $orderModel = M('points:pointsOrder');
+
+            $info = $orderModel->select("*")->where("outpu_time > " . time() . " and status = 5 and goods_id =" . $goods_id)->order("outpu_time desc")->getOne();
+
+            if (!empty($info)) {
+
+                $cache->set($key, $info['pur_id'], $info['outpu_time'] - time());
+                return $info['pur_id'];
+            } else {
+                return false;
+            }
+        }
+    }
+
+    //判断积分是否足够
+    public function checkSupply($user_id, $num = null)
+    {
+
+        if ($pointsRow['cate_id'] == 7) {
+            return false;
+        }
+        $user = M('public:common')->model('contact_info');
+        $info = $user->where("user_id=$user_id")->getRow();
+        if(empty($info))
+        {
+            return false;
+        }
+        if (($info['quan_points'] - $num) < 0) {
+            return false;
+        }
+        return true;
+
+
+    }
+
 }
