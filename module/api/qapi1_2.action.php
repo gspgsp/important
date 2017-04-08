d69246c68f96a875eed23709e9ea152d4d142193
xielei <xielei@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-04-08 02:55:31 +0000

    bug  fixed

--- module/api/qapi1_2.action.php
+++ module/api/qapi1_2.action.php
@@ -651,6 +651,14 @@ class qapi1_2Action extends null2Action
 
         if($contact_id){
             $top= M ('qapp:plasticPersonalInfo')->getMyOwnInfo($contact_id);
+            foreach($data['data'] as $key=>&$val)
+            {
+                if($val['user_id']==$top['user_id'])
+                {
+                    unset($data['data'][$key]);
+                }
+            }
+            unset($val);
         }
         if ($page == 1) {
             $members = M ('qapp:plasticPersonalInfo')->getAllMembers ();
@@ -838,12 +846,25 @@ class qapi1_2Action extends null2Action
             //只有在有置顶头条并且页面是首页或者智能推荐时候有效
             if($pur_id &&($sortField1 == 'ALL'||$sortField2 == 'AUTO'|| $sortField2 == 'DEMANDORSUPPLY')){
 
-                $top = M("qapp:plasticRelease")->model('purchase')->where("id = $pur_id")->getRow();
-                $top = M("qapp:plasticRelease")->model ('purchase')->select ('pur.id,pur.p_id,pur.user_id,pro.model,pur.unit_price,pur.store_house,fa.f_name,pur.input_time,pur.type,pur.content')->from ('purchase pur')
+                $top = M ('qapp:plasticRelease')->getReleaseMsgDetail($pur_id,$user_id,$user_id);
+                $personal = M ('qapp:plasticPersonalInfo')->getMyOwnInfo($user_id);
+                $_tmp = $top['info'];
+                unset($top['info']);
+                $top =  array_merge($top,$_tmp,$personal);
+
+                foreach($data['data'] as $key=>&$val)
+                {
+                    if($val['id']==$top['id'])
+                    {
+                        unset($data['data'][$key]);
+                    }
+                }
+                unset($val);
+               /* $top = M("qapp:plasticRelease")->model ('purchase')->select ('pur.id,pur.p_id,pur.user_id,pro.model,pur.unit_price,pur.store_house,fa.f_name,pur.input_time,pur.type,pur.content')->from ('purchase pur')
                     ->leftjoin ('product pro', 'pur.p_id=pro.id')
                     ->leftjoin ('factory fa', 'pro.f_id=fa.fid')
                     ->leftjoin ('contact_info info', 'info.user_id=pur.user_id')
-                    ->where("pur.id = $pur_id")->getRow();
+                    ->where("pur.id = $pur_id")->getRow();*/
                 $arr = array( 'err' => 0, 'data' => $data['data'],'top'=>$top );
             }
 
