97946d324a9c947ac60d69defc74536654c0943e
cuiyinming <cuiyinming@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-05-12 07:33:45 +0000

    release

--- module/qapi_3/toutiao.action.php
+++ module/qapi_3/toutiao.action.php
@@ -9,7 +9,7 @@ class toutiaoAction extends baseAction
 {
 
     /**
-     * @api {post} /qapi_3/toutiao/topLine 塑料头条测试999997测试画面太美丽了
+     * @api {post} /qapi_3/toutiao/topLine 塑料头条测试
      * @apiVersion 3.1.0
      * @apiName  topLine
      * @apiGroup toutiao
