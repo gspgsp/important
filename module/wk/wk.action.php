2f0d6faf40441f7caa951d33850e31fe2933118c
cuiyinming <cuiyinming@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-05-12 08:18:12 +0000

    release

--- module/wk/wk.action.php
+++ module/wk/wk.action.php
@@ -1,6 +1,6 @@
 <?php
 
-class indexAction extends adminBaseAction{
+class wkAction extends adminBaseAction{
 
 	protected $model,$uname,$today,$db,$adminid;
 	public function __init(){
@@ -215,7 +215,7 @@ class indexAction extends adminBaseAction{
 	    *@param $data    一个二维数组,结构如同从数据库查出来的数组
 	    *@param $title   excel的第一行标题,一个数组,如果为空则没有标题
 	    *@param $filename 下载的文件名
-	    *@examlpe 
+	    *@examlpe
 	    *$stu = M ('User');
 	    *$arr = $stu -> select();
 	    *exportexcel($arr,array('id','账户','密码','昵称'),'文件名!');
@@ -224,7 +224,7 @@ class indexAction extends adminBaseAction{
 	{
 	    header("Content-type:application/octet-stream");
 	    header("Accept-Ranges:bytes");
-	    header("Content-type:application/vnd.ms-excel");  
+	    header("Content-type:application/vnd.ms-excel");
 	    header("Content-Disposition:attachment;filename=".$filename.".xls");
 	    header("Pragma: no-cache");
 	    header("Expires: 0");
@@ -242,7 +242,7 @@ class indexAction extends adminBaseAction{
 	                $data[$key][$ck]=iconv("UTF-8", "GB2312", $cv);
 	            }
 	            $data[$key]=implode("\t", $data[$key]);
-	            
+
 	        }
 	        echo implode("\n",$data);
 	    }
