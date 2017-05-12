415918e00e65839743bf109ce6b6eea0bf1ef173
cuiyinming <cuiyinming@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-05-12 07:12:17 +0000

    auto

--- module/qapi_3/toutiao.action.php
+++ module/qapi_3/toutiao.action.php
@@ -9,7 +9,7 @@ class toutiaoAction extends baseAction
 {
 
     /**
-     * @api {post} /qapi_3/toutiao/topLine 塑料头条ssuuuusssssss2
+     * @api {post} /qapi_3/toutiao/topLine 塑料头条ssu6666666ssss2
      * @apiVersion 3.1.0
      * @apiName  topLine
      * @apiGroup toutiao
