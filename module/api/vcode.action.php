63def8848ccdc45deac6ce0e7f19ac40003eb248
xielei <xielei@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-05-16 03:34:57 +0000

    auto release |获取验证码借口文档

--- module/api/vcode.action.php
+++ module/api/vcode.action.php
@@ -36,7 +36,7 @@ class vcodeAction extends homeBaseAction
 
     /**
      * 验证验证码
-     * @api {get} /api/vcode 验证码 供我的塑料网前端与塑料圈H5 PC端使用
+     * @api {get} api/vcode/chkVcode 验证验证码 供我的塑料网前端与塑料圈H5 PC端使用
      * @apiVersion 3.1.0
      * @apiName  chkVcode
      * @apiGroup api
