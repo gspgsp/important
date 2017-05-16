521b6e652d59e65a94d316c6096339f3d2e5c681
xielei <xielei@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-05-16 03:14:44 +0000

    auto release |获取验证码借口文档

--- module/api/vcode.action.php
+++ module/api/vcode.action.php
@@ -11,7 +11,7 @@ class vcodeAction extends homeBaseAction
     }
     /**
      * 获取验证
-     * @api {get} /api/vcode 获取验证 供我的塑料网前端
+     * @api {get} /api/vcode 获取验证 供我的塑料网前端与塑料圈H5 PC端使用
      * @apiVersion 3.1.0
      * @apiName  vcode
      * @apiGroup api
@@ -35,9 +35,29 @@ class vcodeAction extends homeBaseAction
     }
 
     /**
-     * 检查验证码
-     * @access public
-     * @return html
+     * 验证验证码
+     * @api {get} /api/vcode 验证码 供我的塑料网前端与塑料圈H5 PC端使用
+     * @apiVersion 3.1.0
+     * @apiName  chkVcode
+     * @apiGroup api
+     *
+     * @apiSampleRequest http://test.myplas.com/api/vcode/chkVcode?name=regcode&value=4587
+     * @apiParam   {String} name  值regcode
+     * @apiParam   {String} value  4322
+     *
+     * @apiSuccess {int}  err   错误码
+     * @apiSuccess {String}   msg   描述
+     *
+     * @apiSuccessExample {json} Success-Response:
+         *{
+        "err": 0,
+        "msg": "验证成功"
+        }
+     * @apiErrorExample {json} Error-Response:
+     *     {
+     *       "err": 1,
+     *       "msg": "验证码输入不正确"
+     *      }
      */
     public function chkVcode ()
     {
