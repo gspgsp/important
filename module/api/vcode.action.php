dbbc1fc711b567f483a3c5a39ef8dc938bea09bd
xielei <xielei@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-05-17 09:09:41 +0000

    auto release | app验证码

--- module/api/vcode.action.php
+++ module/api/vcode.action.php
@@ -33,12 +33,52 @@ class vcodeAction extends homeBaseAction
         $vcode->doimage ();
         $name            = 'vc_'.sget ('name', 's', 'vcode');
         if(empty($phonenum)) {
+            $vcode->doimage ();
             $_SESSION[$name] = $vcode->get_code ();
         }else{
             $cache= E('RedisCluster',APP_LIB.'class');
             $cache->set($phonenum.'_'.$name,$vcode->get_code(),300);
         }
         //p ($vcode->get_code ());
+    }
+
+    /**
+     * 获取验证码--仅供APP使用
+     * @api {get} /api/vcode 获取验证码--仅供APP使用
+     * @apiVersion 3.1.0
+     * @apiName  vcode
+     * @apiGroup api
+     *
+     * @apiParam   {int} phonenum  手机号 APP端传此参数
+     * @apiParam   {int} code  手机号 APP端传此参数
+     *
+     * @apiSampleRequest http://test.myplas.com/api/vcode
+     */
+    public function app ()
+    {
+        $vcode            = new vcode();
+        $vcode->code_len  = 4;
+        $vcode->font_size = 14;
+        $vcode->width     = 80;
+        $vcode->height    = 36;
+        $vcode->seedtype  = 2;
+        //$vcode->background = "#cccccc";
+        ini_set ('display_errors', 'On');
+        $key = md5(time().rand(0,100));
+        $name = FILE_URL.'/myapp/vcode/'.$key.'.png';
+        $path = '../static/myapp/vcode/'.$key.'.png';
+
+        $vcode->do_file_image ($path);
+        $code = $vcode->get_code();
+
+        $cache= E('RedisCluster',APP_LIB.'class');
+        $cache->set($key,$code,300);
+
+        $this->json_output(array(
+            'img'=>$name,
+            'key'=>$key
+        ));
+
     }
 
     /**
@@ -51,7 +91,7 @@ class vcodeAction extends homeBaseAction
      * @apiSampleRequest http://test.myplas.com/api/vcode/chkVcode
      * @apiParam   {String} name  值regcode
      * @apiParam   {String} value  4322
-     * @apiParam   {int} phonenum  手机号  APP端传此参数
+     * @apiParam   {String} key  手机号  APP端传此参数
      *
      * @apiSuccess {int}  err   错误码
      * @apiSuccess {String}   msg   描述
@@ -71,9 +111,9 @@ class vcodeAction extends homeBaseAction
     {
         $name  = sget ('name', 's');
         $value = sget ('value', 's');
-        $phonenum= sget('phonenum','i',0);
+        $key   = sget('key','i',0);
         $value = strtolower ($value);
-        if(empty($phonenum)) {
+        if(empty($key)) {
             if (!chkVcode ($name, $value)) {
                 $this->error ('验证码输入不正确');
             } else {
@@ -81,7 +121,7 @@ class vcodeAction extends homeBaseAction
             }
         }else{
             $cache= E('RedisCluster',APP_LIB.'class');
-            $code = $cache->get($phonenum.'_'.$name);
+            $code = $cache->get($key);
             if(empty($code)||$code!=$value)
             {
                 $this->error ('验证码输入不正确');
