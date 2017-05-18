c5eff29bc106cb120ed18653be16e5df853faef0
cuiyinming <cuiyinming@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-05-18 07:29:39 +0000

    release

--- module/product/spdf.action.php
+++ module/product/spdf.action.php
@@ -11,7 +11,7 @@ class spdfAction extends adminBaseAction {
 	 *
 	 */
 	public function tihuo(){
-			$contract = L('pdf_temp')['test'];
+			$contract = L('pdf_temp.test');
 			p($contract);die;
 			// foreach($detiles as $k => $v){
 			// 	$sign = round($v['number']*$v['unit_price'],2);
