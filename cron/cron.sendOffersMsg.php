b6230eba3a7a3535491be09ba5c1eda7908e6d8c
yezhongbao <yezhongbao@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-05-24 09:42:40 +0000

    报价发送公海客户 release

--- cron/cron.sendOffersMsg.php
+++ cron/cron.sendOffersMsg.php
@@ -49,7 +49,7 @@ class cronsendOffersMsg{
 			$this->db->model('offers_cron')->where('id='.$value['id'])->update(array('status'=>1));
 		}
 		$count = $this->db->model('customer')->getOne("SELECT COUNT(c_id) FROM `p2p_customer`
-		WHERE customer_manager>0  AND `STATUS` <> 9 AND (need_product <> '' OR need_product_adm <> '')");
+		WHERE `STATUS` <> 9 AND (need_product <> '' OR need_product_adm <> '')");
 		$this->nlimit=1000; //每次查询1000条
 		$nums = ceil($count/$this->nlimit);
 		foreach ($res as $key => $value) {
@@ -62,7 +62,7 @@ class cronsendOffersMsg{
 		}
 	}
 	public function getCustomerContact($i,$offers_info){
-		$res = $this->db->model('customer')->select('c_id,c_name,need_product_adm,need_product')->where("customer_manager>0 and status <> 9 and msg = 2 and (need_product <> '' OR need_product_adm <> '') AND china_area = '".$offers_info['china_area']."'")->limit($i*$this->nlimit.",".$this->nlimit)->getAll();
+		$res = $this->db->model('customer')->select('c_id,c_name,need_product_adm,need_product,customer_manager')->where("status <> 9 and msg = 2 and (need_product <> '' OR need_product_adm <> '') AND china_area = '".$offers_info['china_area']."'")->limit($i*$this->nlimit.",".$this->nlimit)->getAll();
 		// p($offers_info);die;
 		// showtrace();
 			foreach ($res as $key => $value) {
@@ -90,11 +90,18 @@ class cronsendOffersMsg{
 				array_push($same_arr, $offers_info['grade']);//将主牌号加入发布牌号数组
 				$arr_res = array_intersect($same_arr,$need_product);//发布牌号数组与所需牌号数组交集
 				if($arr_res){
-					$this->addMsgLog($value['c_id'],$offers_info);
+					//处理发送对象，一种是发私海+合作，一种是发公海（对应业务员是发布报价的业务员）
+					if($value['customer_manager'] == 0){
+						//公海客户区别就是一个customer_manager = 0
+						$this->addPublicMsgLog($value['c_id'],$offers_info);
+					}else{
+						//私海客户+合作客户（区别就是一个customer_manager <> 0）
+						$this->addSelfMsgLog($value['c_id'],$offers_info);
+					}
 				}
 		}
 	}
-	public function addMsgLog($c_id,$offers_info){
+	public function addSelfMsgLog($c_id,$offers_info){
 		$res = $this->db->model('customer')->getAll("SELECT c1.`user_id`,c1.`name` as contact_name,c1.`c_id`,c1.`mobile` AS contact_mobile,customer_manager,adm.`name`,adm.`mobile` FROM `p2p_customer_contact` AS c1
 			LEFT JOIN p2p_admin AS adm ON c1.`customer_manager` = adm.`admin_id`
 			LEFT JOIN `p2p_adm_role_user` AS `user` ON `user`.`user_id` = c1.`customer_manager`
@@ -107,14 +114,65 @@ class cronsendOffersMsg{
 		$date = date("m月d日",time());
 		foreach ($res as $key => $value) {
 			if(is_mobile($value['contact_mobile'])){
-				// p($offers_info);die;
+				// 相同报价，相同联系人，只发送一次
+				$same_sms_res = $this->checkSameMobile($value['contact_mobile'],$offers_info['offers_id']);
+				if(!empty($same_sms_res)){
+					continue;
+				}
 				if(!empty($offers_info['remark'])){
 					$offers_info['remark'] = trim($offers_info['remark'],'。').'。';
 				}
 				$msg = sprintf(L('offers_sms.offers'),$date,$offers_info['factory'],$offers_info['grade'],$offers_info['store'],$offers_info['sale_price'].'元/吨',$value['name'],$value['mobile'],$offers_info['remark']);
 				// p($msg);die;
 	    		M('system:sysSMS')->send($value['user_id'],$value['contact_mobile'],$msg,12,0,$offers_info['offers_id']);
+	    		//添加到号码去重日志中
+	    		$this->addSmsLog($value['contact_mobile'],$offers_info['offers_id']);
 	    	}
+		}
+	}
+	public function addPublicMsgLog($c_id,$offers_info){
+		$res = $this->db->model('customer')->getAll("SELECT c1.`user_id`,c1.`name` AS contact_name,c1.`c_id`,c1.`mobile` AS contact_mobile FROM `p2p_customer_contact` AS c1
+			WHERE c1.`status` = 1 AND c1.`name` <> '' AND c1.`mobile` <> '' AND c1.`c_id` =".$c_id);
+		// showtrace();
+		if(!$res){
+			return;
+		}
+		$date = date("m月d日",time());
+		foreach ($res as $key => $value) {
+			if(is_mobile($value['contact_mobile'])){
+				// 相同报价，相同联系人，只发送一次
+				$same_sms_res = $this->checkSameMobile($value['contact_mobile'],$offers_info['offers_id']);
+				if(!empty($same_sms_res)){
+					continue;
+				}
+				if(!empty($offers_info['remark'])){
+					$offers_info['remark'] = trim($offers_info['remark'],'。').'。';
+				}
+				//报价信息，拿出报价中的报价人和报价人手机号
+				$offer_res = $this->db->model('offers_msg')->where('id='.$offers_info['offers_id'])->getRow();
+				$msg = sprintf(L('offers_sms.offers'),$date,$offers_info['factory'],$offers_info['grade'],$offers_info['store'],$offers_info['sale_price'].'元/吨',$offer_res['uname'],$offer_res['person_phone'],$offers_info['remark']);
+	    		M('system:sysSMS')->send($value['user_id'],$value['contact_mobile'],$msg,12,0,$offers_info['offers_id']);
+	    		
+	    		//添加到号码去重日志中
+	    		$this->addSmsLog($value['contact_mobile'],$offers_info['offers_id']);
+	    	}
+		}
+	}
+	public function checkSameMobile($mobile,$offers_id){
+		$where = 'mobile='.$mobile.' and offers_id = '.$offers_id.' and ('.time().'- input_time) < 600';
+		$res = $this->db->model('offers_sms_log')->where($where)->getRow();
+		return $res;
+	}
+	public function addSmsLog($mobile,$offers_id){
+		$res = $this->db->model('offers_sms_log')->where('mobile='.$mobile.' and offers_id='.$offers_id)->getRow();
+		if($res){
+			$this->db->model('offers_sms_log')
+    			->update(array('id'=>$res['id'],'input_time'=>time()));
+		}else{
+    		$this->db->model('offers_sms_log')
+    			->add(array('offers_id'=>$offers_id,
+    						'mobile'=>$mobile,
+    						'input_time'=>time()));
 		}
 	}
 }
