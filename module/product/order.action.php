ca395836f3898c8b77ec734dcce56378e73c8dbe
cuiyinming <cuiyinming@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-05-25 02:31:10 +0000

    release

--- module/product/order.action.php
+++ module/product/order.action.php
@@ -175,12 +175,9 @@ class orderAction extends adminBaseAction {
 			$where.=" and `o_id` in ($oids)";
 		}elseif(!empty($keyword) && $key_type=='m_code'){
 			$oids = M('product:outStorage')->getColByInfo($keyword,'ol.o_id','car_code');
-			showtrace();
 			$where.=" and `o_id` in ($oids)";
 		}elseif(!empty($keyword) && ($key_type=='m_driver' || $key_type=='m_idcode')){
 			$oids = M('product:outStorage')->getColByInfo($keyword,'ol.o_id','driver');
-			echo 1;
-			showtrace();
 			$where.=" and `o_id` in ($oids)";
 		}elseif(!empty($keyword)){
 			$where.=" and `$key_type` like '%".$keyword."%'";
