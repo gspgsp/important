e75df0137153ccbeb50c87683d373e57d36efd31
zhanpeng <zhanpeng@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-05-27 08:52:04 +0000

    auto release

--- module/qapi_3/pay.action.php
+++ module/qapi_3/pay.action.php
@@ -15,88 +15,74 @@ class payAction extends baseAction
          E ('multiPay', APP_LIB.'class');
     }
 
-       public function getPrePayOrder(){
+    /**
+     * 网上支付
+     * @api {post} /qapi_3/pay/getPrePayOrder 网上支付-获取预定义订单
+     * @apiVersion 3.1.0
+     * @apiName  getPrePayOrder
+     * @apiGroup pay
+     * @apiUse UAHeader
+     *
+     * @apiParam {String} token       token
+     * @apiParam {int} type       1  weixin zhifu     2支付宝
+     * @apiParam {Number} goods_id       商品id（非必填）  //  为了以后的商品，也用支付，现在我们只用来充塑豆 ，传total_fee
+     * @apiParam {Number} goods_num       商品数量（非必填）
+     * @apiParam {String} total_fee       总金额  单位元（必填）
+     *
+     *
+     *
+     * @apiSuccess {String}  msg   描述
+     * @apiSuccess {Boolean} err   错误码
+     * @apiSuccess {Array} data   信息
+     *
+     * @apiSuccessExample Success-Response:
+     *     {
+     *      "err":0
+     *      "info":"xxx"
+     *      }
+     *
+     *
+     */
 
-           $user_id = $this->checkAccount();
-           $type = sget('type','i',1);
-           $goods_id = sget('goods_id','i',12);
-           $goods_num = sget('goods_num','i',1);
+   public function getPrePayOrder(){
 
-           $order_id = $this->buildOrderId ();
-           $send_amount = $amount = 10;
-           if($type == 'weixin')
-           {
-               $send_amount = $amount*100;
-           }
-          /* $url = "https://api.mch.weixin.qq.com/pay/unifiedorder";
-           $notify_url = $this->config["notify_url"];
+       $user_id = $this->checkAccount();
+       $type = sget('type','i',1);   // 1 weixin   2  支付宝
+       $goods_id = sget('goods_id','i');
+       $goods_num = sget('goods_num','i');
+       $total_fee = sget('total_fee','s');//  单位为元
+       $total_fee = sprintf("%.2f", $total_fee);
+       $total_fee = $total_fee + 0;
 
-           $onoce_str = $this->getRandChar(32);
+       if(empty($goods_id) && empty($goods_num) && empty($total_fee)){
+           $this->_errCode(6);
+       }
+       if(empty($goods_id) && empty($goods_num) && !empty($total_fee)){
+           $msg = $this->goods_name;
+       }
 
-           $data["appid"] = $this->config["appid"];
-           $data["body"] = $body;
-           $data["mch_id"] = $this->config['mch_id'];
-           $data["nonce_str"] = $onoce_str;
-           $data["notify_url"] = $notify_url;
-           $data["out_trade_no"] = $out_trade_no;
-           $data["spbill_create_ip"] = $this->get_client_ip();
-           $data["total_fee"] = $total_fee;
-           $data["trade_type"] = "APP";
-           $s = $this->getSign($data, false);
-           $data["sign"] = $s;
-
-           $xml = $this->arrayToXml($data);
-           $response = $this->postXmlCurl($xml, $url);
-
-           //将微信返回的结果xml转成数组
-           //return $this->xmlstr_to_array($response);*/
+       if($type == 1)
+       {
+           $order_id =$this->buildOrderId ();
+           $send_amount = 0;
+           $send_amount = (int)($total_fee*100);
            $this->payment = new multiPay($type);
            $res = $this->payment->getPrePayOrder($order_id, $send_amount);
 
            $order  = M('order:onlineOrder');
 
-           $data = $order ->addOrder($order_id,$type,$res['prepay_id'],$amount,$goods_id,$goods_num,$user_id,$this->uuid,$res['appid'],$this->platform,$res['status'],$res['remark']);
+           $data = $order ->addOrder($order_id,$type,$res['prepay_id'],$total_fee,$goods_id,$goods_num,$user_id,$this->uuid,$res['appid'],$this->platform,$res['status'],$res['remark']);
 
-           //showTrace();
-           if($res['status'] == 0&&!empty($data))
-           {
-               $tmp['order_id'] = $order_id;
-               $tmp['partner_order_sn'] = $res['prepay_id'];
-
-               $this->json_output (array(
-                   'err' => 0,
-                   'msg' => '订单生成成功',
-                   'data'=> $tmp
-               ));
+           if($res['status'] == 0&&!empty($data)) {
+               $x = $this->payment->getOrder($res['prepay_id']);
+               $this->json_output(array('err'=>0,'msg'=>'订单生成成功','data'=>$x));
            }else{
-               $this->_errCode(1001);
+               $this->json_output(array('err'=>1,'msg'=>'订单生成失败'));
            }
+       }else{
+           $this->_errCode(5);
        }
-
-
-    public function getOrder($prepayId){
-        /*$data["appid"] = $this->config["appid"];
-        $data["noncestr"] = $this->getRandChar(32);;
-        $data["package"] = "Sign=WXPay";
-        $data["partnerid"] = $this->config['mch_id'];
-        $data["prepayid"] = $prepayId;
-        $data["timestamp"] = time();
-        $s = $this->getSign($data, false);
-        $data["sign"] = $s;*/
-        $type = sget('type','s','wechatpay');
-        $prepayId = sget('prepayId','s');
-        $this->pay = new multiPay($type);
-
-        $res = $this->pay->$type->getOrder($prepayId);
-
-        $this->json_output (array(
-            'err'  => 0,
-            'res' => $res,
-        ));
-    }
-
-
-
+   }
 
 
 
