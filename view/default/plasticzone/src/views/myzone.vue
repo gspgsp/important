8cd3eb4af028274e1727726ab0c8f79693f03cdb
zhouziheng <zhouziheng@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-06-05 10:14:34 +0000

    release

--- view/default/plasticzone/src/views/myzone.vue
+++ view/default/plasticzone/src/views/myzone.vue
@@ -228,34 +228,34 @@ export default{
 
 		});
 		
-		$.ajax({
-			url:version+'/pay/getPrePayOrder',
-			type:'post',
-			data:{
-				type:1,
-				total_fee:"0.01"
-			},
-			headers: {
-				'X-UA': window.localStorage.getItem("XUA")
-			},
-			dataType: 'JSON'
-		}).done(function(res){
-			console.log(res);
-			if(res.err==0){
-				wx.chooseWXPay({
-				    timestamp: res.data.timestamp,
-				    nonceStr: res.data.noncestr,
-				    package: "prepay_id="+res.data.prepayid,
-				    signType: 'MD5',
-				    paySign: res.data.sign,
-				    success: function (data) {
-				        console.log(">>>",data);
-				    }
-				});			
-			}
-		}).fail(function(){
-			
-		});
+//		$.ajax({
+//			url:version+'/pay/getPrePayOrder',
+//			type:'post',
+//			data:{
+//				type:1,
+//				total_fee:"0.01"
+//			},
+//			headers: {
+//				'X-UA': window.localStorage.getItem("XUA")
+//			},
+//			dataType: 'JSON'
+//		}).done(function(res){
+//			console.log(res);
+//			if(res.err==0){
+//				wx.chooseWXPay({
+//				    timestamp: res.data.timestamp,
+//				    nonceStr: res.data.noncestr,
+//				    package: "prepay_id="+res.data.prepayid,
+//				    signType: 'MD5',
+//				    paySign: res.data.sign,
+//				    success: function (data) {
+//				        console.log(">>>",data);
+//				    }
+//				});			
+//			}
+//		}).fail(function(){
+//			
+//		});
 
 		$.ajax({
 			url: version+'/myInfo/myZone',
