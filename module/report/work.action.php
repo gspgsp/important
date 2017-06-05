caa6b2bf882534112bf412b31bec74c9f1bde9de
cuiyinming <cuiyinming@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-06-05 08:06:04 +0000

    release

--- module/report/work.action.php
+++ module/report/work.action.php
@@ -7,6 +7,9 @@ class workAction extends adminBaseAction {
 	// private $title='后台用户信息列表'; //
 	public function __init(){
 		$this->debug = false;
+		$this->assign('type',L('company_type'));//工厂类型
+		$this->assign('cus_status',L('cus_status'));//客户状态
+		$this->assign('belong_area',L('belong_area'));//所属区域
 		// $this->db=M('public:common')->model('work');
 	}
 	/**
