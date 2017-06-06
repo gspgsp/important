a0dc3b2e8938b0135438550de3f5e6c72d9f7035
xielei <xielei@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-06-06 01:53:46 +0000

    auto release | 微信支付替换成沙箱流程 apikey完成修改

--- class/wechatPay.class.php
+++ class/wechatPay.class.php
@@ -26,15 +26,15 @@ class wechatPay{
         $notify_url = $this->config["notify_url"];
         $onoce_str = $this->getRandChar(32);
 
-        /*$data["appid"] = $this->config["appid"];
-        $data["body"] = $body;*/
+        $data["appid"] = $this->config["appid"];
+        $data["body"] = $body;
         $data["mch_id"] = $this->config['mch_id'];
         $data["nonce_str"] = $onoce_str;
-        /*$data["notify_url"] = $notify_url;
+        $data["notify_url"] = $notify_url;
         $data["out_trade_no"] = $out_trade_no;
         $data["spbill_create_ip"] = $this->get_client_ip();
         $data["total_fee"] = $total_fee;
-        $data["trade_type"] = "APP";*/
+        $data["trade_type"] = "APP";
         $s = $this->getSign($data);
         $data["sign"] = $s;
 
