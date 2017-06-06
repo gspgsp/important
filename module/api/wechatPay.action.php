9b3166506504baa3d8fee12251bfe522cd726fa9
xielei <xielei@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-06-06 08:56:02 +0000

    auto release | wechat 通知回调

--- module/api/wechatPay.action.php
+++ module/api/wechatPay.action.php
@@ -53,6 +53,7 @@ class wechatPayAction extends null2Action
         $sign           = strtoupper (md5 ($stringSignTemp));
         $order_info     = M ('order:onlineOrder')->getPk ($data['out_trade_no']);
         file_put_contents('/tmp/xielei.txt',print_r($sign,true)."//////sign\n",FILE_APPEND);
+        file_put_contents('/tmp/xielei.txt',print_r($order_info,true)."//////$order_info\n",FILE_APPEND);
 
         if ($sign == $data['sign']) {
             //            $msg = date("Y-m-d H:i:s")." 支付通知验签通过\n";
