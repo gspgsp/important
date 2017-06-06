7a608d6d7eda715fcc7a1ba3eab08c064c168ca0
xielei <xielei@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-06-06 08:18:13 +0000

    auto release | webchat

--- class/wechatPay.class.php
+++ class/wechatPay.class.php
@@ -48,10 +48,8 @@ class wechatPay{
         $s = $this->getSign($data);
         $data["sign"] = $s;
 
-        file_put_contents('/tmp/xielei.txt',print_r($data,true)."\n",FILE_APPEND);
         $xml = $this->arrayToXml($data);
         $response = $this->postXmlCurl($xml, $url);
-        file_put_contents('/tmp/xielei.txt',print_r($response,true)."\n",FILE_APPEND);
 
         //将微信返回的结果xml转成数组
         return $this->xmlstr_to_array($response);
