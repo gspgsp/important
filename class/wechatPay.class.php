53907a870b471b69499c9dcfec1a397390a92273
xielei <xielei@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-06-06 01:27:20 +0000

    auto release | 微信支付替换成沙箱流程 完成版DEBUG

--- class/wechatPay.class.php
+++ class/wechatPay.class.php
@@ -35,8 +35,10 @@ class wechatPay{
         $s = $this->getSign($data, false);
         $data["sign"] = $s;
 
+        file_put_contents('/tmp/xielei.txt',print_r($data,true)."\n",FILE_APPEND);
         $xml = $this->arrayToXml($data);
         $response = $this->postXmlCurl($xml, $url);
+        file_put_contents('/tmp/xielei.txt',print_r($this->xmlstr_to_array($response),true)."\n",FILE_APPEND);
 
         //将微信返回的结果xml转成数组
         return $this->xmlstr_to_array($response);
