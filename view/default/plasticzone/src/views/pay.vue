765d5ac8f0b5f0c25f93492d0e7a17e9b240e1ff
zhouziheng <zhouziheng@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-06-07 10:05:45 +0000

    release

--- view/default/plasticzone/src/views/pay.vue
+++ view/default/plasticzone/src/views/pay.vue
@@ -77,11 +77,9 @@ methods: {
 			},
 			dataType: 'JSON'
 		}).done(function(res) {
-			console.log(">>>",JSON.parse(res.data));
 			if(res.err == 0) {
 				jsApiParameters={
-					appId:JSON.parse(res.data)
-					
+					JSON.parse(res.data)
 				};
 				if(typeof WeixinJSBridge == "undefined") {
 					if(document.addEventListener) {
