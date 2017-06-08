d3384049a9056d475ee22b3973f02785f41b1f0d
zhanpeng <zhanpeng@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-06-08 07:22:19 +0000

    release

--- class/WxpayAPI_php_v3/example/notify.php
+++ class/WxpayAPI_php_v3/example/notify.php
@@ -2,13 +2,13 @@
 ini_set('date.timezone','Asia/Shanghai');
 error_reporting(E_ERROR);
 
-require_once "../lib/WxPay.Api.php";
-require_once '../lib/WxPay.Notify.php';
-require_once 'log.php';
+require_once dirname(__FILE__)."/../lib/WxPay.Api.php";
+require_once dirname(__FILE__).'/../lib/WxPay.Notify.php';
+//require_once 'log.php';
 
 //初始化日志
-$logHandler= new CLogFileHandler("../logs/".date('Y-m-d').'.log');
-$log = Log::Init($logHandler, 15);
+//$logHandler= new CLogFileHandler("../logs/".date('Y-m-d').'.log');
+//$log = Log::Init($logHandler, 15);
 
 class PayNotifyCallBack extends WxPayNotify
 {
@@ -18,7 +18,7 @@ class PayNotifyCallBack extends WxPayNotify
 		$input = new WxPayOrderQuery();
 		$input->SetTransaction_id($transaction_id);
 		$result = WxPayApi::orderQuery($input);
-		Log::DEBUG("query:" . json_encode($result));
+//		Log::DEBUG("query:" . json_encode($result));
 		if(array_key_exists("return_code", $result)
 			&& array_key_exists("result_code", $result)
 			&& $result["return_code"] == "SUCCESS"
@@ -32,9 +32,9 @@ class PayNotifyCallBack extends WxPayNotify
 	//重写回调处理函数
 	public function NotifyProcess($data, &$msg)
 	{
-		Log::DEBUG("call back:" . json_encode($data));
+		//Log::DEBUG("call back:" . json_encode($data));
 		$notfiyOutput = array();
-		
+
 		if(!array_key_exists("transaction_id", $data)){
 			$msg = "输入参数不正确";
 			return false;
@@ -48,6 +48,6 @@ class PayNotifyCallBack extends WxPayNotify
 	}
 }
 
-Log::DEBUG("begin notify");
-$notify = new PayNotifyCallBack();
-$notify->Handle(false);
+////Log::DEBUG("begin notify");
+//$notify = new PayNotifyCallBack();
+//$notify->Handle(false);
