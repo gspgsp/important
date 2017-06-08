911ec6c6478d84c2185e6e9fc8f4eec84e5e0281
zhanpeng <zhanpeng@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-06-08 07:23:51 +0000

    release

--- module/api/wechatPay.action.php
+++ module/api/wechatPay.action.php
@@ -165,7 +165,7 @@ class wechatPayAction extends null2Action
         require_file(APP_LIB."class/WxpayAPI_php_v3/example/notify.php");
         chmod('./sjs.txt',0777);
         file_put_contents('./sjs.txt',print_r('this is a b',true)."\n",FILE_APPEND);
-        $notify = new PayNotifyCallBack();p(a);p($notify);
+        $notify = new PayNotifyCallBack();
         file_put_contents('./sjs.txt',print_r('s',true)."\n",FILE_APPEND);
         $notify->Handle(false);
         file_put_contents('./sjs.txt',print_r('---------------------------',true)."\n",FILE_APPEND);
