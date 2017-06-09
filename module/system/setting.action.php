931a3962eaa45963474119b23cea21c5650dac83
xielei <xielei@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-06-09 06:36:40 +0000

    auto release |后台debug

--- module/system/setting.action.php
+++ module/system/setting.action.php
@@ -194,6 +194,7 @@ class settingAction extends adminBaseAction {
 
 			var_dump(1231432423);
 			$this->model('setting')->where("code='qapp_newest_version'")->update(array('value'=>$_POST['qapp_newest_version']));
+			echo $this->model('setting')->getLastSql();
 			M('system:setting')->del_cache('setting_qapp_newest_version');
 			M('system:setting')->del_cache('setting');
 			var_dump(111111111);
