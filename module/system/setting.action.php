6d97874c24b19bf00101d10b37efcb4f6ca8f264
xielei <xielei@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-06-09 06:53:11 +0000

    auto release |后台debug

--- module/system/setting.action.php
+++ module/system/setting.action.php
@@ -193,7 +193,7 @@ class settingAction extends adminBaseAction {
 			}
 
 			var_dump(1231432423);
-			$this->db->model('setting')->where("code=qapp_newest_version")->update(array('value'=>$_POST['qapp_newest_version']));
+			$this->db->model('setting')->where("`code`='qapp_newest_version'")->update(array('value'=>$_POST['qapp_newest_version']));
 			echo $this->db->model('setting')->getLastSql();
 			M('system:setting')->del_cache('setting_qapp_newest_version');
 			M('system:setting')->del_cache('setting');
