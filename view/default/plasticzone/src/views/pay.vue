1ba4ae0e7e68d315c90b8cda24e62d9e0b221f7f
zhouziheng <zhouziheng@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-06-09 07:17:40 +0000

    release

--- view/default/plasticzone/src/views/pay.vue
+++ view/default/plasticzone/src/views/pay.vue
@@ -1,36 +1,36 @@
 <template>
 <div>
-<header id="bigCustomerHeader">
-	<a class="back" href="javascript:window.history.back();"></a>
-	充值中心
-</header>
-<div class="payWrap">
-	<div style="padding: 0 12px;">
-		<h3 class="payTitle">充值金额:</h3>
-		<ul class="payList">
-			<li v-bind:class="{on:index==eq?true:false}" v-for="(p,index) in pay" v-on:click="paySelect(p.plasticBean,p.money,index)">
-				<div class="payBox">
-					<span>{{p.money}}元</span><br>{{p.plasticBean}}塑豆
-				</div>
-			</li>
-		</ul>
-		<div class="payOther">
-			<span>其他金额：</span>
-			<input class="payNum" type="tel" v-model="inputMoney" v-on:input="payInput" placeholder="15" />
-			<b>{{plasticBean}}</b>
+	<header id="bigCustomerHeader">
+		<a class="back" href="javascript:window.history.back();"></a>
+		充值中心
+	</header>
+	<div class="payWrap">
+		<div style="padding: 0 12px;">
+			<h3 class="payTitle">充值金额:</h3>
+			<ul class="payList">
+				<li v-bind:class="{on:index==eq?true:false}" v-for="(p,index) in pay" v-on:click="paySelect(p.plasticBean,p.money,index)">
+					<div class="payBox">
+						<span>{{p.money}}元</span><br>{{p.plasticBean}}塑豆
+					</div>
+				</li>
+			</ul>
+			<div class="payOther">
+				<span>其他金额：</span>
+				<input class="payNum" type="tel" v-model="inputMoney" v-on:input="payInput" placeholder="15" />
+				<b>{{plasticBean}}</b>
+			</div>
+		</div>
+		<div class="payWay">
+			<h3>选择支付方式:</h3>
+			<div class="wxPay">
+				<i class="iconWxPay"></i>微信支付
+				<i class="iconWxRight"></i>
+			</div>
+		</div>
+		<div style="padding: 0 15px; margin: 30px 0 0 0;">
+			<div class="wxPayBtn" v-on:click="payMoney">支付</div>
 		</div>
 	</div>
-	<div class="payWay">
-		<h3>选择支付方式:</h3>
-		<div class="wxPay">
-			<i class="iconWxPay"></i>微信支付
-			<i class="iconWxRight"></i>
-		</div>
-	</div>
-	<div style="padding: 0 15px; margin: 30px 0 0 0;">
-		<div class="wxPayBtn" v-on:click="payMoney">支付</div>
-	</div>
-</div>
 </div>
 </template>
 <script>
@@ -42,205 +42,245 @@ export default {
 			eq: null,
 			inputMoney: null,
 			plasticBean: "",
-			order_id:"",
-			beanOrder:{
-				bean:"",
-				money:""
+			order_id: "",
+			beanOrder: {
+				bean: "",
+				money: ""
 			}
-	}
-},
-methods: {
-	payMoney: function() {
-		var _this = this;
-		var jsApiParameters={};
-		function onBridgeReady() {
-			console.log("11111");
-			WeixinJSBridge.invoke(
-				'getBrandWCPayRequest',
-				jsApiParameters,
-				function(res) {
-					if(res.err_msg == "get_brand_wcpay_request:ok") {
-						$.ajax({
-							type:"post",
-							url:version + '/pay/updateOrderStatus',
-							data: {
-								type: 1,
-								order_id:_this.order_id,
-								status:"4"
-							},
-							headers: {
-								'X-UA': window.localStorage.getItem("XUA")
-							},
-							dataType: 'JSON'
-						}).then(function(res){
+		}
+	},
+	methods: {
+		payMoney: function() {
+			var _this = this;
+			var jsApiParameters = {};
+
+			function onBridgeReady() {
+				console.log("11111");
+				WeixinJSBridge.invoke(
+					'getBrandWCPayRequest',
+					jsApiParameters,
+					function(res) {
+						if(res.err_msg == "get_brand_wcpay_request:ok") {
 							
-						},function(){
+							weui.alert('支付成功', {
+								title: '塑料圈通讯录',
+								buttons: [{
+									label: '确定',
+									type: 'primary',
+									onClick: function() {
+										
+									}
+								}]
+							});
 							
-						});		
+							$.ajax({
+								type: "post",
+								url: version + '/pay/updateOrderStatus',
+								data: {
+									type: 1,
+									order_id: _this.order_id,
+									status: "4"
+								},
+								headers: {
+									'X-UA': window.localStorage.getItem("XUA")
+								},
+								dataType: 'JSON'
+							}).then(function(res) {
+
+							}, function() {
+
+							});
+						}
+						if(res.err_msg == "get_brand_wcpay_request:cancel") {
+							
+							weui.alert('支付取消', {
+								title: '塑料圈通讯录',
+								buttons: [{
+									label: '确定',
+									type: 'primary',
+									onClick: function() {
+										
+									}
+								}]
+							});
+
+							$.ajax({
+								type: "post",
+								url: version + '/pay/updateOrderStatus',
+								data: {
+									type: 1,
+									order_id: _this.order_id,
+									status: "-3"
+								},
+								headers: {
+									'X-UA': window.localStorage.getItem("XUA")
+								},
+								dataType: 'JSON'
+							}).then(function(res) {
+
+							}, function() {
+
+							});
+						}
+						if(res.err_msg == "get_brand_wcpay_request:fail") {
+							
+							weui.alert('支付失败', {
+								title: '塑料圈通讯录',
+								buttons: [{
+									label: '确定',
+									type: 'primary',
+									onClick: function() {
+										
+									}
+								}]
+							});
+
+							$.ajax({
+								type: "post",
+								url: version + '/pay/updateOrderStatus',
+								data: {
+									type: 1,
+									order_id: _this.order_id,
+									status: "-4"
+								},
+								headers: {
+									'X-UA': window.localStorage.getItem("XUA")
+								},
+								dataType: 'JSON'
+							}).then(function(res) {
+
+							}, function() {
+
+							});
+						}
 					}
-					if(res.err_msg == "get_brand_wcpay_request:cancel"){
-						$.ajax({
-							type:"post",
-							url:version + '/pay/updateOrderStatus',
-							data: {
-								type: 1,
-								order_id:_this.order_id,
-								status:"-3"
-							},
-							headers: {
-								'X-UA': window.localStorage.getItem("XUA")
-							},
-							dataType: 'JSON'
-						}).then(function(res){
-							
-						},function(){
-							
-						});		
-					}
-					if(res.err_msg == "get_brand_wcpay_request:fail"){
-						$.ajax({
-							type:"post",
-							url:version + '/pay/updateOrderStatus',
-							data: {
-								type: 1,
-								order_id:_this.order_id,
-								status:"-4"
-							},
-							headers: {
-								'X-UA': window.localStorage.getItem("XUA")
-							},
-							dataType: 'JSON'
-						}).then(function(res){
-							
-						},function(){
-							
-						});								
+				);
+			}
+
+			$.ajax({
+				url: version + '/pay/getPrePayOrder',
+				type: 'post',
+				data: {
+					type: 1,
+					goods_id: "99",
+					total_fee: _this.beanOrder.money,
+					goods_num: _this.beanOrder.bean,
+					open_id: window.localStorage.getItem("openid")
+				},
+				headers: {
+					'X-UA': window.localStorage.getItem("XUA")
+				},
+				dataType: 'JSON'
+			}).done(function(res) {
+				if(res.err == 0) {
+					_this.order_id = res.order_id;
+					jsApiParameters = JSON.parse(res.data);
+					if(typeof WeixinJSBridge == "undefined") {
+						if(document.addEventListener) {
+							document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+						} else if(document.attachEvent) {
+							document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+							document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+						}
+					} else {
+						onBridgeReady();
 					}
 				}
-			);
-		}
 
+			}).fail(function() {
+
+			});
+		},
+		paySelect: function(num, money, i) {
+			this.beanOrder.bean = num;
+			this.beanOrder.money = money;
+			this.money = money;
+			this.eq = i;
+			this.inputMoney = null;
+			this.plasticBean = null;
+
+		},
+		payInput: function() {
+			var _this = this;
+			this.eq = null;
+			if(this.inputMoney==0){
+				this.inputMoney="";
+				this.plasticBean = "";
+				this.paySelect(100, 10, 0);
+			}else if(this.inputMoney>0 && this.inputMoney <= 10000) {
+				$.ajax({
+					type: "post",
+					url: version + "/pay/getExactAmount",
+					data: {
+						money: _this.inputMoney
+					},
+					headers: {
+						'X-UA': window.localStorage.getItem("XUA")
+					},
+					dataType: 'JSON'
+				}).then(function(res) {
+					if(res.err == 0) {
+						_this.beanOrder.bean = res.plasticBean;
+						_this.beanOrder.money = _this.inputMoney;
+						_this.plasticBean = res.plasticBean + "塑豆";
+					}
+
+				}, function() {
+
+				});
+			} else if(this.inputMoney > 10000) {
+				this.inputMoney = 10000;
+				$.ajax({
+					type: "post",
+					url: version + "/pay/getExactAmount",
+					data: {
+						money: _this.inputMoney
+					},
+					headers: {
+						'X-UA': window.localStorage.getItem("XUA")
+					},
+					dataType: 'JSON'
+				}).then(function(res) {
+					if(res.err == 0) {
+						_this.plasticBean = res.plasticBean + "塑豆";
+						_this.beanOrder.bean = res.plasticBean;
+						_this.beanOrder.money = _this.inputMoney;
+
+					}
+
+				}, function() {
+
+				});
+			} else {
+				this.plasticBean = "";
+				this.paySelect(100, 10, 0);
+			}
+
+		}
+	},
+	activated: function() {
+		try {
+			var piwikTracker = Piwik.getTracker("http://wa.myplas.com/piwik.php", 2);
+			piwikTracker.trackPageView();
+		} catch(err) {
+
+		}
+		var _this = this;
 
 		$.ajax({
-			url: version + '/pay/getPrePayOrder',
-			type: 'post',
+			type: "post",
+			url: version + "/pay/getPayConfig",
 			data: {
-				type: 1,
-				goods_id: "99",
-				total_fee: _this.beanOrder.money,
-				goods_num: _this.beanOrder.bean,
-				open_id: window.localStorage.getItem("openid")
+
 			},
 			headers: {
 				'X-UA': window.localStorage.getItem("XUA")
 			},
 			dataType: 'JSON'
-		}).done(function(res) {
-			if(res.err == 0) {
-				_this.order_id=res.order_id;
-				jsApiParameters=JSON.parse(res.data);
-				if(typeof WeixinJSBridge == "undefined") {
-					if(document.addEventListener) {
-						document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-					} else if(document.attachEvent) {
-						document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
-						document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-					}
-				} else {
-					onBridgeReady();
-				}
-			}
-
-		}).fail(function() {
-
-		});
-	},
-	paySelect: function(num, money, i) {
-		this.beanOrder.bean=num;
-		this.beanOrder.money=money;
-		this.money = money;
-		this.eq = i;
-		this.inputMoney = null;
-		this.plasticBean = null;
-
-	},
-	payInput: function() {
-		var _this = this;
-		this.eq = null;
-		if(this.inputMoney && this.inputMoney <= 10000) {
-			$.ajax({
-				type: "post",
-				url: version + "/pay/getExactAmount",
-				data: {
-					money: _this.inputMoney
-				},
-				headers: {
-					'X-UA': window.localStorage.getItem("XUA")
-				},
-				dataType: 'JSON'
-			}).then(function(res) {
-				if(res.err == 0) {
-					_this.beanOrder.bean=res.plasticBean;
-					_this.beanOrder.money=_this.inputMoney;
-					_this.plasticBean = res.plasticBean + "塑豆";
-				}
-
-			}, function() {
-
-			});
-		} else if(this.inputMoney > 10000) {
-			this.inputMoney = 10000;
-			$.ajax({
-				type: "post",
-				url: version + "/pay/getExactAmount",
-				data: {
-					money: _this.inputMoney
-				},
-				headers: {
-					'X-UA': window.localStorage.getItem("XUA")
-				},
-				dataType: 'JSON'
-			}).then(function(res) {
-				if(res.err == 0) {
-					_this.plasticBean = res.plasticBean + "塑豆";
-					_this.beanOrder.bean= res.plasticBean;
-					_this.beanOrder.money=_this.inputMoney;
-
-				}
-
-			}, function() {
-
-			});
-		} else {
-			this.plasticBean = "";
-			this.paySelect(100,10, 0);
-		}
-
-	}
-},
-activated: function() {
-	try {
-		var piwikTracker = Piwik.getTracker("http://wa.myplas.com/piwik.php", 2);
-		piwikTracker.trackPageView();
-	} catch(err) {
-
-	}
-	var _this = this;
-
-	$.ajax({
-		type: "post",
-		url: version + "/pay/getPayConfig",
-		data: {
-
-		},
-		headers: {
-			'X-UA': window.localStorage.getItem("XUA")
-		},
-		dataType: 'JSON'
 		}).then(function(res) {
 			if(res.err == 0) {
 				_this.pay = res.data;
-				_this.paySelect(res.data[0].plasticBean,res.data[0].money, 0);
+				_this.paySelect(res.data[0].plasticBean, res.data[0].money, 0);
 			}
 
 		}, function() {
