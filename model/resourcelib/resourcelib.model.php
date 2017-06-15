b5a2d2e23a42c57f4aa1ad8b1004334dd0fe2cc0
xielei <xielei@42e704b2-0b0d-4f99-8aec-43dd1e6149ab>
2017-06-15 03:14:36 +0000

    资源

--- model/resourcelib/resourcelib.model.php
+++ model/resourcelib/resourcelib.model.php
@@ -82,5 +82,91 @@ class resourcelibModel extends model{
 		$where = "id in ($ids)";
 		return $this->where($where)->order('input_time desc')->getAll();
 	}
-
+	/**
+	*资源库-根据牌号搜索
+	*/
+	public function getSearchResource($place,$province,$PlsticNumber,$start=0,$limit=0,$Time,$ISForward){
+		
+		$where_gi = "";$where_qi = " qi.Isbuy=0";
+		if(!empty($PlsticNumber)){
+			$where_gi .= " gi.PlsticNumber = '".$PlsticNumber."'";
+			$where_qi .= " AND qi.PlsticNumber = '".$PlsticNumber."'";
+			
+		}
+		$where_qi .= " AND qi.UpdateTime > '".$Time."'";
+		
+		if(!empty($PlsticNumber) && !empty($province)){
+			$where_gi .= " AND";
+		}else{
+			$where_gi .= "";
+		}
+		if(!empty($province)){
+			$where_gi .= " gi.GoodssPosition in (".$province.")";
+		}
+		if((!empty($province) || !empty($PlsticNumber)) && !empty($place)){
+			$where_gi .=" AND";
+		}else{
+			$where_gi .= "";
+		}
+		if(!empty($place)){
+			$num = count($place);
+			$where_gi .= " ( gi.Production like '%$place[0]%'";
+			$where = $where_gi;
+			if($num > 1){
+				for($i=1;$i<$num;$i++){
+					$where_gi .= " OR gi.Production like '%$place[$i]%'";
+				}
+			}
+			$where_gi .= " )";
+		}
+		if((!empty($place) || !empty($province) || !empty($PlsticNumber)) && !empty($ISForward)){
+			$where_gi .= " AND gi.ISForward = '".$ISForward."'";
+		}else{
+			$where_gi .= "";
+		}		
+		$goods_company['resource'] = $this->from('goods_information as gi')
+							->select('gi.Company,gi.GoodssPosition,gi.PlsticNumber,gi.ISForward,gi.Price,gi.Production')
+							->where($where_gi)->group('gi.Company')->limit("$start,$limit")->getAll();
+		$gi_company = array();
+		if(!empty($goods_company['resource'])){
+			foreach($goods_company['resource'] as $key=>$val){
+				$arr = array();
+				$position = $this->from('goods_information')->select('GoodssPosition')->where("Company='".trim($val['Company'])."'")->getAll();//公司牌号所有的地区
+				
+				$arr = array_unique(array_column($position,'GoodssPosition'));
+				$goods_company['resource'][$key]['GoodssPosition'] = $arr;
+				$gi_company[$key] = "'".$val['Company']."'";
+			}
+			$where_qi .= " AND qi.Company in (".implode(',',$gi_company).")";
+		}
+		
+		$qi_company = $this->from('qq_information as qi')
+							->select('qi.QQName,qi.QQNumber,qi.QQImage,qi.Company,qi.UpdateTime')
+							->where($where_qi)->getAll();
+				//showTrace();
+		foreach($goods_company['resource'] as $key=>$val){
+			$user_info = $this->user_info($val['Company']);
+			if(!empty($user_info)){
+				foreach($user_info as $k=>$v){
+					$goods_company['resource'][$key]['Iphone_list'][] = trim($v['UserName']).' '.trim($v['Iphone']);
+					$goods_company['resource'][$key]['Iphone'] = trim($v['Iphone']);
+				}
+			}
+			
+			foreach($qi_company as $k=>$v){
+				if(trim($val['Company']) == trim($v['Company'])){
+					$goods_company['resource'][$key]['QQName'][] = $v['QQName'];
+					$goods_company['resource'][$key]['QQNumber'][] = $v['QQNumber'];
+					$goods_company['resource'][$key]['QQImage'][] = $v['QQImage'];
+					$goods_company['resource'][$key]['UpdateTime'] = $v['UpdateTime'];
+				}
+			}
+		}
+		//p($goods_company);die;
+		$goods_company['total'] = $this->from('goods_information as gi')->select('count(*) as total')->where($where_gi)->getOne();
+		return $goods_company;
+	}
+	public function user_info($Company){
+		return $this->from('user_information')->select('UserName,Iphone')->where("Company='".trim($Company)."'")->getAll();
+	}
 }
